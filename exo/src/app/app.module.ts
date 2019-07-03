a6201f9b4cbc2e13e545114776d914827bb4ccc6
laKalash <51700862+laKalash@users.noreply.github.com>
2019-07-03 11:37:48 +0200

    Add files via upload

---
+++ exo/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { PiedComponent } from './pied/pied.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ListeActuComponent } from './liste-actu/liste-actu.component';
+import { ActuComponent } from './actu/actu.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { ActuService } from './services/actu.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    PiedComponent,
+    AccueilComponent,
+    ListeActuComponent,
+    ActuComponent,
+    ConnexionComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [ActuService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
